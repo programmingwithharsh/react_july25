f3cc8cf8a6f3ad1f42368aba0f485d0c6264f1ef
Harsh Tiwari <harshtiwari343@email.com>
2025-07-16 12:20:58 +0530

    With and Without JSX

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
-// import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render("Programming with Harsh");
+
+const message = 'Programming with Harsh';
+const flowers = ["Rose", "Dahlia", "Magnolia", "Tulip", "Daisy"]; // array
+
+// Without JSX
+const element1 = React.createElement("h1", {}, "Programming with Harsh");
+const element2 = React.createElement("ul",
+    {},
+    flowers.map((item, index) => React.createElement("li", { key: index }, item))
+)
+
+// With JSX, we can directly write HTML code inside Js
+const element3 = <h1>Programming with Harsh 2 + 2 is {2 + 2}</h1>;
+const element4 = <ul>
+    {flowers.map((item, index) => (
+        <li key={index}>{item}</li>
+    ))}
+</ul>
+
+root.render(element4);
