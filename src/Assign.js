7d45ccf711fc4b1a103672b8e2047a3c3f450ac7
Harsh Tiwari <harshtiwari343@email.com>
2025-07-16 13:02:13 +0530

    Update Assignment

---
+++ src/Assign.js
@@ -0,0 +1,22 @@
+/*
+
+Assignment 3 - Create Class or Functional Components in react 
+
+Create Class or Functional Components in react
+
+Create Main Class Component and call Main component inside index.js file
+Create below components and call inside Main Component
+Welcome -> Display Welcome message class or functional component 
+Login  -> Display Login message class or functional component 
+Register -> Display Register message class or functional component 
+LoginSuccessful -> Display LoginSuccessful message class or functional component 
+RegisterSuccessful -> Display RegisterSuccessful message class or functional component 
+ChatList -> Display ChatList message class or functional component 
+UserList -> Display UserList message class or functional component 
+EditUser  -> Display EditUser message class or functional component 
+DocumentList -  -> Display DocumentList message class or functional component 
+Logout -> Display Logout message class or functional component 
+For reference - https://github.com/programmingwithharsh/react_july25/
+Note Once completed, add code in the private comment and mark as done, Format code
+
+*/
