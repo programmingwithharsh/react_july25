ac4710462f7b2892c966707aa0aad91a9a1259d5
Harsh Tiwari <harshtiwari343@email.com>
2025-07-16 13:51:34 +0530

    Update State

--- src/Main.js
+++ src/Main.js
@@ -6,9 +6,26 @@ import Title from './Title';
 import Star from './Star';
 
 export default class Main extends React.Component {
+    constructor() {
+        super();
+        console.log("Constructor - 1");
+        this.state = {
+            address: "Mumbai",
+            hobbies: ["Singing", "Music", "Dance", "Chess"]
+        }
+    }
+
+    updateAddress = () => {
+        this.setState({ address: "Delhi" })
+    }
+
     render() { // Class Component Lifecycle
+        console.log("Render - 2");
+        console.log(this.state);
         return <div>
             <h1>This is Main Class Component</h1>
+            <h2>State is, I am living in {this.state.address}</h2>
+            <button onClick={this.updateAddress}>Update Address</button>
             <ProductList />
             <AddProduct />
             <Welcome />
