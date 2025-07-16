b28ac58a494f16e17862814d52853b2b233a2cda
Harsh Tiwari <harshtiwari343@email.com>
2025-07-16 13:04:56 +0530

    Update Assignments

--- src/Assign.js
+++ src/Assign.js
@@ -1,6 +1,45 @@
 /*
 
+Assignment 1 - Create array and object in Javascript
+----------------------------------------------------
+Create array and object in Javascript and print the result in console
+
+1. Create state variable which contains multiple cities (5) for example
+
+state city1, city2, city3, city4, city5
+
+2. Create country object which contains multiple state (5) along with cities (5), for example
+
+country object
+    state city1, city2, city3, city4, city5
+    state city1, city2, city3, city4, city5
+    state city1, city2, city3, city4, city5
+    state city1, city2, city3, city4, city5
+    state city1, city2, city3, city4, city5
+
+Points to remember 
+Don't use space in object key
+In case of multiple words for object key, for example Madhya Pradesh use either camelcase madhyaPradesh or underscore madhya_pradesh in between words.
+Format code before submit it.
+Use actual state and city name
+
+Assignment 2 - Display your name in browser using react javascript library
+--------------------------------------------------------------------------
+Display your name in browser using react
+
+Delete all files from the src folder except index.js, inside this file write your name to display in browser, for example 
+
+index.js file
+
+import ReactDOM from 'react-dom/client';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+"Programming with Harsh"
+);
+
 Assignment 3 - Create Class or Functional Components in react 
+-------------------------------------------------------------
 
 Create Class or Functional Components in react
 
