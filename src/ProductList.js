446cdbfea4b374feafd12ad301efb6c3bfd4ff93
Harsh Tiwari <harshtiwari343@email.com>
2025-07-16 12:56:42 +0530

    Class and Functional Component

---
+++ src/ProductList.js
@@ -0,0 +1,8 @@
+import Product from './Product';
+
+export default function ProductList() {
+    return <div>
+        <h1>This is Product List Functional Component</h1>
+        <Product />
+    </div>
+}
